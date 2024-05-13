12703950a5ebb7125a9157137dcbc918af3d6245
Tatiana <155022698+TVAnpilogova@users.noreply.github.com>
2024-05-13 19:06:42 +0300

    last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,22 +1,23 @@
-with tab as( 
-  select 
-    s.visitor_id, --уникальный человек на сайте
-    s.visit_date, --время визита
-    s.source as utm_source,
-    s.medium as utm_medium,
-    s.campaign as utm_campaign, --метки c учетом модели атрибуции
-    l.lead_id, --идентификатор лида, если пользователь сконвертился в лид после(во время) визита, NULL — если пользователь не оставил лид
-    l.created_at, --время создания лида, NULL — если пользователь не оставил лид
-    l.amount, --сумма лида (в деньгах), NULL — если пользователь не оставил лид
-    l.closing_reason, --причина закрытия, NULL — если пользователь не оставил лид
-    l.status_id, --код причины закрытия, NULL — если пользователь не оставил лид
-    row_number() over(partition by s.visitor_id order by s.visit_date desc) as number
-  from sessions as s
-  join leads as l on
-  s.visitor_id=l.visitor_id
-  WHERE s.source != 'organic' AND s.medium != 'organic' AND s.campaign != 'organic'
-)
-  select
+with tab as(
+    select
+        s.visitor_id, --уникальный человек на сайте
+        s.visit_date, --время визита
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign, --метки c учетом модели атрибуции
+        l.lead_id, --идентификатор лида
+        l.created_at, --время создания лида
+        l.amount, --сумма лида (в деньгах)
+        l.closing_reason, --причина закрытия
+        l.status_id, --код причины закрытия
+        row_number() over(partition by s.visitor_id order by s.visit_date desc) as number
+    from sessions as s
+    join
+        leads as l on
+            s.visitor_id = l.visitor_id
+    WHERE s.source != 'organic' AND s.medium != 'organic' AND s.campaign != 'organic'
+    )
+    select
     visitor_id, 
     visit_date,
     utm_source,
